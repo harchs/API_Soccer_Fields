4edd9eee6429961caaa13285b20aade34497bc3a
Harold Rangel Charris <harold.rangel@koombea.com>
2012-03-26 10:53:25 -0500

    [feature] generating credential for user logged

--- app/models/user_keys.rb
+++ app/models/user_keys.rb
@@ -1,3 +1,22 @@
 class UserKeys < ActiveRecord::Base
 	belongs_to :users
+
+	def self.save_keys(params)
+		create! do |user|
+			user.uid = params["uid"]
+			user.token = params["token"]
+			user.app_id = params["app_id"]
+			user.user_id = params["user_id"]
+		end
+	end
+
+	def self.disable_credential!
+    	self.update_attribute(:credential, "")
+  	end
+
+  	def save_credential(credential)
+    	self.update_attribute(:credential, credential)
+  	end
+
+
 end
--- app/models/users.rb
+++ app/models/users.rb
@@ -1,3 +1,15 @@
+
 class Users < ActiveRecord::Base
 	has_many :user_keys
+
+	def self.save_user(params)
+		create! do |user|
+			user.first_name = params["first_name"]
+			user.last_name = params["last_name"]
+			user.nick_name = params["nick_name"]
+			user.email = params["email"]
+		end
+	end
+
+	
 end
