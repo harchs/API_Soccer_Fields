e4539f164a9e912eda008ee9a07c6301118f3c73
Harold Rangel Charris <harold.rangel@koombea.com>
2012-03-26 17:06:05 -0500

    [feature] creating validations in user and user_keys model

--- app/models/users.rb
+++ app/models/users.rb
@@ -2,6 +2,10 @@
 class Users < ActiveRecord::Base
 	has_many :user_keys
 
+	validates :first_name, :presence => true
+	validates :last_name, :presence => true
+	validates :email, :format => { :with => /((\S+)@(\S{3}[a-zA-z0-9)]\S*))/, :message => "not valid" } 
+
 	def self.save_user(params)
 		create! do |user|
 			user.first_name = params["first_name"]
--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,25 +2,10 @@ require 'spec_helper'
 
 describe UserController do
 
-  describe "GET 'sign_up'" do
-    it "returns http success" do
-      get 'sign_up'
-      response.should be_success
-    end
+  describe 'params[first_name]' do
+    it "should "
+
   end
 
-  describe "GET 'sign_in'" do
-    it "returns http success" do
-      get 'sign_in'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'sign_out'" do
-    it "returns http success" do
-      get 'sign_out'
-      response.should be_success
-    end
-  end
 
 end
