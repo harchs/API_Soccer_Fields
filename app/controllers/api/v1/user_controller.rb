66e1d49fca5ed891df2478e464abca000fe84227
Leonardo Sanclemente <leonardo.sanclemente@koombea.com>
2012-03-29 10:16:45 -0500

    [config]moving files to v1

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -11,6 +11,7 @@
   param:: uid: uid returned by the outh authentication when this sign in diferentes app (facebook, twitter, ...)
   param:: token: token returned by the outh authentication when this sign in diferentes app (facebook, twitter, ...)
   param:: auth_code: Code generated with private key and modifiers
+  param:: public_key: Public key of the app
   param:: valide_date: [Optional] Specifies whether or not to consider the date to generate the auth_code "aaaa/mm/dd hh"
   param:: modifiers: Modifiers used for encode the private key of the app "modifiers=m1,m2,...mn"
 
@@ -42,6 +43,7 @@
   return:: [JSON] - list of venues objects
 
   param:: credential: Credential asigned to user
+  param:: public_key: Public key of the app
   param:: auth_code: Code generated with private key and modifiers
   param:: valide_date: [Optional] Specifies whether or not to consider the date to generate the auth_code "aaaa/mm/dd hh"
   param:: modifiers: Modifiers used for encode the private key of the app "modifiers=m1,m2,...mn"
